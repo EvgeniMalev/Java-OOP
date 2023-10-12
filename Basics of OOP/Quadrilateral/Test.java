c105d9cb3571b83e5f4a5a4c8d36d75e728ef13a
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2023-10-12 23:03:23 +0300

    Create Test.java

---
+++ Basics of OOP/Quadrilateral/Test.java
@@ -0,0 +1,17 @@
+package javabsoop;
+
+import bsoop.*;
+
+class Test extends Main
+{
+    
+   public static void main(String args[])
+   {
+       
+	Test obj = new Test();
+	
+	System.out.println(obj.sumTwoNumbers(2, 4, 5, 3));
+	
+   }
+   
+}
