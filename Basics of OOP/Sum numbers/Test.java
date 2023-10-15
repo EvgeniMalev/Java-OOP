c6a8f6b7be04b4b351c260ccc4c2edf3abf16fe3
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2023-10-15 20:37:44 +0300

    Create Test.java

---
+++ Basics of OOP/Sum numbers/Test.java
@@ -0,0 +1,15 @@
+package javasum;
+
+import sum.*;
+
+class Test extends sumNumbers {
+    
+   public static void main(String args[]) {
+       
+	Test obj = new Test();
+	
+	System.out.println(obj.sumNumbers(3, 8));
+	
+   }
+   
+}
