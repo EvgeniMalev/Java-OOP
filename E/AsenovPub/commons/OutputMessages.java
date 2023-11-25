be28cf99bc8b683ff73501992a7db7f05de303f8
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2023-11-25 20:06:46 +0200

    Create OutputMessages.java

---
+++ E/AsenovPub/commons/OutputMessages.java
@@ -0,0 +1,16 @@
+package asenovpub.common;
+
+public class OutputMessages {
+
+    public static final String WINE_ADDED = "You choose wine  %s!";
+
+    public static final String RAKIA_ADDED = "You choose rakia %s!";
+
+    public static final String FOOD_ADDED = "For lunch You choose %s!";
+	
+	  public static final String MUSIC_ADDED = "You are listening ... ";
+
+    public static final String BILL = "Booth: %d%nBill: %.2f";
+
+    public static final String TOTAL_INCOME = "Income: %.2flv";
+}
