95d53945002dc34b113ab26d9050ecf44c9b8a3f
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2024-02-13 23:28:11 +0200

    Create Melothria.java

---
+++ Class/Еxotic Vegetables/subclasses/Melothria.java
@@ -0,0 +1,18 @@
+
+package exoticvegetable;
+
+Melothria.java
+
+public class Melothria extends ExoticVegetable {
+    public Melothria() {
+        super("Mexico", "Melothria", 4);
+    }
+
+    public void produceTendrils() {
+        System.out.println("Melothria produces tendrils.");
+    }
+
+    public void bearFruits() {
+        System.out.println("Melothria bears fruits.");
+    }
+}
