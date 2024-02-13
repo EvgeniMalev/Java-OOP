78ad1495c92e8050e50d9b38e6cb630a81c57185
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2024-02-13 23:26:21 +0200

    Create RomanescoBroccoli.java

---
+++ Class/Еxotic Vegetables/subclasses/RomanescoBroccoli.java
@@ -0,0 +1,9 @@
+package exoticvegetable;
+
+RomanescoBroccoli.java
+
+public class RomanescoBroccoli extends ExoticVegetable {
+    public RomanescoBroccoli() {
+        super("Roman empire", "Brassica oleracea var. botrytis", 5);
+    }
+}
