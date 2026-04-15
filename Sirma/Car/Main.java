dda99a220c75fdd53006ac16faa2349e7757a3f6
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2026-04-15 23:39:05 +0300

    Create Main.java

---
+++ Sirma/Car/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = Integer.parseInt(scanner.nextLine());
+        
+        List<Car> cars = new ArrayList<>();
+        
+
+        for (int i = 0; i < n; i++) {
+            String[] carDetails = scanner.nextLine().split(" ");
+            String brand = carDetails[0];
+            String model = carDetails[1];
+            int horsepower = Integer.parseInt(carDetails[2]);
+            
+            Car car = new Car(brand, model, horsepower);
+            cars.add(car);
+        }
+        
+        for (Car car : cars) {
+            System.out.println(car.carInfo());
+        }
+
+        scanner.close();
+    }
+}
